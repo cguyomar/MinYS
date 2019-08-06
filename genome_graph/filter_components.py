8ef1a4ea44fa84926e379f398db3dab31dda7e37
cguyomar <cervin.guyomar@gmail.com>
2019-08-06 13:19:06 +0200

    Add utility for filtering out small connected components

---
+++ genome_graph/filter_components.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+"""
+Remove all connected components of a gfa graph smaller than a threshold
+"""
+
+import argparse
+
+import genome_graph
+
+op = argparse.ArgumentParser()
+op.add_argument("infile")
+op.add_argument("outfile")
+op.add_argument("minlength")
+opts = op.parse_args()
+
+print("Loading graph")
+g = genome_graph.GenomeGraph.read_gfa(opts.infile) 
+
+for comp in g.connected_components():
+    #print(comp)
+    compLength = 0
+    for n in comp:
+        compLength = compLength + len(g.nodes[n].nodeSeq)  # We should not count the overlaps twice in the length  computation
+    if compLength < int(opts.minlength):
+        for n in comp:
+            g.rem_node(n)
+
+g.write_gfa(opts.outfile)
+
