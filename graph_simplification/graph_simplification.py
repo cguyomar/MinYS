5e31ab4177ffc8524d5b87705e1631053f7ad7eb
cguyomar <cervin.guyomar@gmail.com>
2020-04-16 17:26:47 +0200

    Add graph splitting to the simplification pipeline

--- graph_simplification/graph_simplification.py
+++ graph_simplification/graph_simplification.py
@@ -12,6 +12,7 @@ from genome_graph import genome_graph
 
 op = argparse.ArgumentParser()
 op.add_argument("-l",default=100,help="Length of minimal suffix for node merging",type=int)
+op.add_argument("-s",default=5,help="Cut graph at nodes branching with at leasdt s edges",type=int)
 
 op.add_argument("infile")
 op.add_argument("outfile")
@@ -27,6 +28,9 @@ print("\n")
 
 print("Popping bubbles")
 g.pop_all_bubbles()
+
+print("Splitting graph")
+g.split_branching_nodes(opts.s)
 
 print("Merging linear paths")
 g.merge_all_linear_paths()
